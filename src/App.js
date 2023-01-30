28b43e0753f428a0b8ca6aa8df70147a15638d10
Archamedis <104657642+Archamedis@users.noreply.github.com>
2023-01-30 22:19:39 +0000

    Mission 5: HOORAH!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-function App() {
-  return (
-    <div className="app">
+import { ColorModeContext, useMode } from "./theme"; // how the fuck do these imports work WTF?! https://www.youtube.com/watch?v=g5o-YqOHQM8
+// defaults outside bracket. Anything with keyword `export` in function call within bracket.
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
 
-    </div>
+function App() {
+  const [theme, colorMode] = useMode(); // we have access to the theme and colormode now
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline /> 
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
